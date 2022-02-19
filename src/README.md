7f215554fcdea5c13004dac90580cfa3678b83ae
huakwan <kwanpon@gmail.com>
2022-02-19 23:27:03 +0700

    Version 4.8.1

---
+++ src/README.md
@@ -0,0 +1,9 @@
+# Adminer
+
+Adminer - Database management in a single PHP file
+
+https://www.adminer.org/
+
+Supports: MySQL, MariaDB, PostgreSQL, SQLite, MS SQL, Oracle, SimpleDB, Elasticsearch, MongoDB, Firebird
+Requirements: PHP 5+
+Apache License 2.0 or GPL 2
---
+++ src/index.php
@@ -0,0 +1,30 @@
+<?php
+
+function adminer_object()
+{
+    // required to run any plugin
+    include_once "./plugins/plugin.php";
+
+    // autoloader
+    foreach (glob("plugins/*.php") as $filename) {
+        include_once "./$filename";
+    }
+
+    $plugins = [
+        new AdminerDatabaseHide([
+            'mysql',
+            'information_schema',
+            'performance_schema',
+            'sys'
+        ]),
+        new AdminerDisplayForeignKeyName(),
+        new AdminerTheme(),
+    ];
+
+    return new AdminerPlugin($plugins);
+}
+
+// include original Adminer
+include "./adminer.php";
+
+?>
